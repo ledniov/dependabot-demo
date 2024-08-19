388ff947907c76d7f9d68f2f99960aa2a5e85c42
Serhii Lednov <serge.ledniov@gmail.com>
2024-08-19 12:20:55 +0000

    update renovate configs, add more examples to track

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.43.0"
+    }
+  }
+}
+
+# Provider configuration
+provider "aws" {
+    region = "us-west-2"
+}
+
+# RDS instance
+resource "aws_db_instance" "example_rds" {
+    identifier             = "example-rds"
+    engine                 = "postgres"
+    engine_version         = "12.7"
+    instance_class         = "db.t2.micro"
+    allocated_storage      = 20
+    storage_type           = "gp2"
+    username               = "admin"
+    password               = "password"
+    publicly_accessible   = false
+}
+
+# Lambda function
+resource "aws_lambda_function" "example_lambda" {
+    function_name = "example-lambda"
+    runtime       = "nodejs12.x"
+    handler       = "index.handler"
+    memory_size   = 128
+    timeout       = 10
+
+    # Add your Lambda function code here
+    # ...
+
+    # Add any necessary IAM permissions here
+    # ...
+}
